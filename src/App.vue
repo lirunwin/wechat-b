cc23a70ed56f1abb54acc784ef02501c69a13a38
pikaryan <lirunw@gmail.com>
2018-06-25 19:28:45 +0800

    :construction:

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '~@/styles/scss/app';
 .container {
-  width: 100%;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  box-sizing: border-box;
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    box-sizing: border-box;
 }
 </style>
--- src/pages/user/signup.vue
+++ src/pages/user/signup.vue
@@ -1,5 +1,6 @@
 <template lang="html">
-  <div>signup</div>
+  <div class="bg-white py-5">懒虫动动
+  <button type="button" class="btn btn-primary">先写按钮</button></div>
 </template>
 
 <script>
